2c2b03e5a5e4f63bfe1b3fe760e849cc5ceae7e0
acgnLiu <liu_wenle_acgn@163.com>
2021-12-03 11:59:50 +0800

    简易框架

---
+++ case/mihoyo_case/mihoyo_test_main.py
@@ -0,0 +1,11 @@
+
+class MihoyoTest(unittest.testCase):
+    
+    def set_up(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def test_main(self):
+        pass
---
+++ page/mihoyo_page/mihoyo_main_page.py
@@ -0,0 +1,7 @@
+from seleniumPython3.base.box import BasePage
+
+
+from seleniumPython3.base.box import BasePage
+
+class MihoyoMainPage(BasePage):
+    pass
